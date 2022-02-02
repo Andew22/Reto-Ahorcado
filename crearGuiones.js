cf63c786a24f99afd89bc23ce7671370c99d9ad4
Andrew22 <agbuitrago2@misena.edu.co>
2022-02-01 22:23:12 -0500

    correccion guion

--- crearGuiones.js
+++ crearGuiones.js
@@ -11,6 +11,7 @@ if(base!=null){
     for (let i = 0; i <numero;i++){
         let input1 = document.createElement("input")
         input1.type="text"
+        input1.disabled="true"
         input1.value=""
         input1.classList.add("estilosGuion")
         base.appendChild(input1);
